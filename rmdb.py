9c189f85a7fc5b212377539e623ddca4fa0b63ea
Sophie Peron <speron@obs-vlfr.fr>
2016-06-08 19:44:34 +0200

    program remove duplicate sequence id

---
+++ rmdb.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+import sys
+
+arguments = sys.argv
+#print (arguments)
+
+InFileName = sys.argv [1]
+InFile = open ( InFileName, 'r' )
+
+#InFileName = 'seqidtest.fa'
+#InFile = open ( InFileName, 'r' )
+
+LineNumbre = 0
+
+lines_seen = set() # holds lines already seen
+for line in InFile:
+	LineNumbre = LineNumbre + 1
+	if LineNumbre > 1:
+		if line not in lines_seen: # not a duplicate
+   			print(line)
+   			lines_seen.add(line)
+
+InFile.close()
